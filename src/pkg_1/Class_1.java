36578633088c531417f23450f492c9c6ad625c87
brian chiem <brianfamily5@gmail.com>
2020-12-29 21:05:39 -0700

    Learn variable rules and how to declare

--- src/pkg_1/Class_1.java
+++ src/pkg_1/Class_1.java
@@ -2,6 +2,26 @@ package JavaSampleProject1.src.pkg_1;
 
 public class Class_1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World.");
+        
+
+        byte b1 = 123;
+
+        short sh1 = 7681;
+
+        int num1 = 12312321;
+
+        long l1 = 4356789;
+
+        float f1 = 12.4567f;
+
+        double d1 = 123.5342534d;
+
+        char ch1 = 'A';
+
+        String str1 = "ASDFDESDF@@3$^&";
+
+        boolean b2 = true;
+
+        System.out.println(b1);
     }
 }
