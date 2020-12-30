1319ae3e0d722e6f357da11549d599342dc00f2f
brian chiem <brianfamily5@gmail.com>
2020-12-30 09:24:15 -0700

    Add ifCondition

---
+++ src/IfCondition.java
@@ -0,0 +1,16 @@
+package JavaSampleProject1.src;
+
+public class IfCondition {
+            // If - Condition "5"
+            public static void main(String[] args) {
+
+            int num1 = 12;
+            int num2 = 20;
+            
+            if (num1 < num2) {
+                System.out.println("num1 is bigger");
+            } else {
+                System.out.println("num1 is not bigger");
+            }
+        }
+}
