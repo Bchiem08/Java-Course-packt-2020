9d67b1aadd53f85b1e3f0aaee5e303e74d649a7f
brian chiem <brianfamily5@gmail.com>
2020-12-30 09:26:19 -0700

    Add relationalOp file

---
+++ src/RelationalOperators.java
@@ -0,0 +1,14 @@
+package JavaSampleProject1.src;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+
+        // Relational Operators in Java "4"
+
+        // == (num1 == num2)
+        // != (num1 != num2)
+        // >  (num1 >= num2)
+        // <  (num1 <= num2)
+        
+    }
+}
