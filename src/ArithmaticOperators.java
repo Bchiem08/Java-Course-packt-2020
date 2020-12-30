45358d70402e5e802be4bd13d204073e37d3d4c7
brian chiem <brianfamily5@gmail.com>
2020-12-30 09:18:53 -0700

    Move arithmatic to seperate file

---
+++ src/ArithmaticOperators.java
@@ -0,0 +1,30 @@
+package JavaSampleProject1.src;
+
+    public class ArithmaticOperators {
+        public static void main(String[] args) {
+            // Arithmetic Operators in Java "2"
+
+        // + - Add
+        // - - Sub
+        // * - Mul
+        // / - Div
+        // % - Mod
+        // ++ - Inc
+        // -- - Dec
+
+        int num1 = 3;
+
+        num1++;
+        num1 = num1 + 1;
+        num1--;
+        num1 = num1 - 1;
+        num1+=5;
+        num1 = num1 + 5;
+        num1-=5;
+        num1 = num1 - 5 ;
+        num1*=2;
+        num1 = num1*2 ;
+        num1/=5;
+        num1 = num1/5 ;
+    }
+}
--- src/Class_1.java
+++ src/Class_1.java
@@ -1,41 +1,20 @@
-package JavaSampleProject1.src.pkg_1;
+package JavaSampleProject1.src;
 public class Class_1 {
     public static void main(String[] args) {
 
-        //Primitive Data Types in Java "1"
+        // Primitive Data Types in Java "1"
 
-        // byte b1 = 123;
-        // short sh1 = 7681;
+        byte b1 = 123;
+        short sh1 = 7681;
         int numb1 = 12312321;
-        // long l1 = 4356789;
-        // float f1 = 12.4567f;
-        // double d1 = 123.5342534d;
-        // char ch1 = 'A';
-        // String str1 = "ASDFDESDF@@3$^&";
-        // boolean b2 = true;
+        long l1 = 4356789;
+        float f1 = 12.4567f;
+        double d1 = 123.5342534d;
+        char ch1 = 'A';
+        String str1 = "ASDFDESDF@@3$^&";
+        boolean b2 = true;
 
-        // Arithmetic Operators in Java "2"
 
-        // + - Add
-        // - - Sub
-        // * - Mul
-        // / - Div
-        // % - Mod
-        // ++ - Inc
-        // -- - Dec
-
-        numb1++;
-        numb1 = numb1 + 1;
-        numb1--;
-        numb1 = numb1 - 1;
-        numb1+=5;
-        numb1 = numb1 + 5;
-        numb1-=5;
-        numb1 = numb1 - 5 ;
-        numb1*=2;
-        numb1 = numb1*2 ;
-        numb1/=5;
-        numb1 = numb1/5 ;
 
         // Logical and Bitwise Operators "3"
 
