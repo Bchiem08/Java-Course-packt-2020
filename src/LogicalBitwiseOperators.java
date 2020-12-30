568278e90467a60a0a33b47acc3cf0102e303279
brian chiem <brianfamily5@gmail.com>
2020-12-30 09:21:21 -0700

    Create Logical bitwise file

---
+++ src/LogicalBitwiseOperators.java
@@ -0,0 +1,17 @@
+package JavaSampleProject1.src;
+
+public class LogicalBitwiseOperators {
+            // Logical and Bitwise Operators "3"
+
+        // Logical AND - &&
+        // Logical OR - ||
+        // Logical Not - !!
+        // Bitwise and - &
+        // Bitwise or - |
+        // Bitwise Compliment _ ~
+
+        int num1 = 100; //111100
+        int num2 = 200; //101010
+                       //101000 => 40
+                       //111110 => 62
+}
