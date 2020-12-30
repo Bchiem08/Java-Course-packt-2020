40e9dcb27ecb3a934aadacfee3061d8d253851be
brian chiem <brianfamily5@gmail.com>
2020-12-30 16:08:27 -0700

    Complete for loop assignment

---
+++ src/NestedForAssign.java
@@ -0,0 +1,22 @@
+package JavaSampleProject1.src;
+
+public class NestedForAssign {
+ 
+    public static void main(String[] args) {
+
+        // Write a program to create pattern
+        
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+
+        for (int i = 1; i <= 6; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("\n");
+		}
+    }
+}
