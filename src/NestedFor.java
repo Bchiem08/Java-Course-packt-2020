16dc8a81864641632de33cfd584265df37f31654
brian chiem <brianfamily5@gmail.com>
2020-12-30 16:03:31 -0700

    Learn nested for loop

---
+++ src/NestedFor.java
@@ -0,0 +1,15 @@
+
+package JavaSampleProject1.src;
+
+public class NestedFor {
+    public static void main(String[] args) {
+    
+    for (int i = 1; i <= 10; i++)
+        for (int j = 1; j <= 5; j++) {
+            System.out.println(i + "," + j);
+        }
+        System.out.println("All Iterations of J are done");
+    }
+}
+    
+    
