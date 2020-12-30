2e0ef1fd34bda1f7b3442a87495992e1324b208e
brian chiem <brianfamily5@gmail.com>
2020-12-30 15:53:25 -0700

    Learn For loop

---
+++ src/ForLoop.java
@@ -0,0 +1,17 @@
+package JavaSampleProject1.src;
+
+public class ForLoop {
+    // For Loop "7"
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if(i % 2 == 0) {
+                System.out.println("Even Number:" + i);
+            }
+      }
+
+        // Program to find Even numbers between 1-100
+        //num % 2
+
+    }
+}
