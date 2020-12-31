8fccd8cd0a172aa6e7548f8d865d038733a6cc24
brian chiem <brianfamily5@gmail.com>
2020-12-30 20:11:30 -0700

    learn String conversions

--- src/Unit2/StringConversion.java
+++ src/Unit2/StringConversion.java
@@ -4,5 +4,33 @@ public class StringConversion {
 
     public static void main(String[] args) {
 
+        // toString
+        // toBinaryString
+        // toHexString
+        // toOctalString
+        // parseInt
+
+        int num1 = 100;
+
+        System.out.println(Integer.toString(num1));
+        System.out.println(Integer.toBinaryString(num1));
+        System.out.println(Integer.toHexString(num1));
+        System.out.println(Integer.toOctalString(num1));
+
+        String str1 = "100";
+        String str2 = "10101010";
+        String str3 = "64";
+        String str4 = "144";
+
+        System.out.println(Integer.parseInt(str1));
+        System.out.println(Integer.parseInt(str2, 2));
+        System.out.println(Integer.parseInt(str3, 16));
+        System.out.println(Integer.parseInt(str4, 8));
+
+
+
+
+
+
     }
 }
