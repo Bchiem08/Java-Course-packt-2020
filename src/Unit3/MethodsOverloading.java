0bd9b9c6abbb6ccd9fb52f151127ffd890b0b302
brian chiem <brianfamily5@gmail.com>
2020-12-31 20:49:11 -0700

    Create methods for addition

--- src/Unit3/MethodsOverloading.java
+++ src/Unit3/MethodsOverloading.java
@@ -1,5 +1,29 @@
 package JavaSampleProject1.src.Unit3;
 
 public class MethodsOverloading {
+
+    /**
+	 * Method Overloading
+	 * Data Type
+	 * Number of Args
+	 * Order
+	 * 
+	 */
+
+    public void addition(int num1, int num2) {
+         System.out.println(num1 + num2);
+     }
+       
+    public void addition(double num1, double num2) {
+        System.out.println(num1 + num2);
+    }
+
+    public void addition(double num1, int num2) {
+        System.out.println(num1 + num2);
+    }
+
+    public void addition(int num1, int num2, int num3) {
+        System.out.println(num1 + num2 + num3);
+    }
     
 }
