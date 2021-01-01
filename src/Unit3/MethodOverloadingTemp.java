0f6c96ed9504e4dbcdb81b64f8e4397bbc78d102
brian chiem <brianfamily5@gmail.com>
2020-12-31 20:54:28 -0700

    Learn method overloading

---
+++ src/Unit3/MethodOverloadingTemp.java
@@ -0,0 +1,17 @@
+package JavaSampleProject1.src.Unit3;
+
+public class MethodOverloadingTemp {
+
+    public static void main(String[] args) {
+
+        MethodsOverloading m2 = new MethodsOverloading();
+        m2.addition(10, 20);
+        m2.addition(12.12, 50);
+        m2.addition(33.33, 44.44);
+        m2.addition(31, 21.23);
+        m2.addition(12, 34, 55);
+
+
+    }
+    
+}
