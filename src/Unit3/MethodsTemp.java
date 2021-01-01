018fc4f985f4809b2128671868ea9c14d8f4e6f4
brian chiem <brianfamily5@gmail.com>
2020-12-31 20:46:07 -0700

    Learn java methods

---
+++ src/Unit3/MethodsTemp.java
@@ -0,0 +1,15 @@
+package JavaSampleProject1.src.Unit3;
+
+public class MethodsTemp {
+
+    public static void main(String[] args) {
+
+        Methods m1 = new Methods();
+        m1.addition(10, 50);
+        m1.doThis();
+        int u = m1.multiply(20, 45);
+        System.out.println(u);
+
+    }
+    
+}
