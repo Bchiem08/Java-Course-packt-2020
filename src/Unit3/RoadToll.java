b678d4cf4aae768cf906b034be1026d02752a382
brian chiem <brianfamily5@gmail.com>
2021-01-01 17:53:05 -0700

    Create variables and method

---
+++ src/Unit3/RoadToll.java
@@ -0,0 +1,22 @@
+package JavaSampleProject1.src.Unit3;
+
+public class RoadToll {
+
+    String type;
+    int tiresCount;
+
+    public void calculateToll() {    
+        if(tiresCount == 2) {
+            System.out.println("Your toll is 0");
+        } else if (tiresCount == 4) {
+            System.out.println("Your toll is 10");
+        } else if (tiresCount > 4) {
+            System.out.println("Your toll is 20");
+        } else {
+            System.out.println("Incorrect tire count");
+        }
+
+
+    }
+    
+}
