4f00474ec232032247831e437ae9a68a9113eae8
brian chiem <brianfamily5@gmail.com>
2021-01-01 18:14:07 -0700

    Learn Data encapsulation

---
+++ src/Unit3/ClassroomMain.java
@@ -0,0 +1,15 @@
+package JavaSampleProject1.src.Unit3;
+
+public class ClassroomMain {
+
+    public static void main(String[] args) {
+
+        Classroom cs1 = new Classroom();
+
+        cs1.setSub("Math");
+        cs1.setStdCount(50);
+        cs1.displayDetails();
+
+    }
+    
+}
---
+++ src/Unit3/DataEncapsulation.java
@@ -0,0 +1,10 @@
+package JavaSampleProject1.src.Unit3;
+
+public class DataEncapsulation {
+
+    /**
+	 * Data Encapsulation also called as Data Hiding
+	 * Hiding the member variables of a class from other classes
+	 */
+    
+}
