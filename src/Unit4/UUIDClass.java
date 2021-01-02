87e4461b25889aafd44d75a0f2f0a4c78562085c
brian chiem <brianfamily5@gmail.com>
2021-01-01 20:16:37 -0700

    Learn how to generate UUID

---
+++ src/Unit4/UUIDClass.java
@@ -0,0 +1,15 @@
+package JavaSampleProject1.src.Unit4;
+
+import java.util.UUID;
+
+public class UUIDClass {
+
+    public static void main(String[] args) {
+
+        UUID.randomUUID();
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(UUID.randomUUID());
+        }
+
+    }
+}
