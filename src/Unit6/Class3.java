34b15545525ba65662fdf415755706c22949a87d
brian chiem <brianfamily5@gmail.com>
2021-01-02 19:50:35 -0700

    Learned Super constructor

---
+++ src/Unit6/Class3.java
@@ -0,0 +1,16 @@
+package JavaSampleProject1.src.Unit6;
+
+public class Class3 {
+
+    int num1;
+
+    public Class3(int a) {
+        num1 = a;
+    }
+    
+    public Class3() {
+        
+    }
+    
+
+}
---
+++ src/Unit6/Class4.java
@@ -0,0 +1,13 @@
+package JavaSampleProject1.src.Unit6;
+
+public class Class4 extends Class3{
+    int num2;
+
+    public Class4(int a, int b) {
+        super(a);
+        num2 = b;
+        
+    }
+
+
+}
