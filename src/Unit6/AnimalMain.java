a07e42d1dedbc4afe3fca6b27efc7c800d578cca
brian chiem <brianfamily5@gmail.com>
2021-01-02 19:40:00 -0700

    Learn poly morphisom

--- src/Unit6/AnimalMain.java
+++ src/Unit6/AnimalMain.java
@@ -4,6 +4,11 @@ public class AnimalMain {
 
     public static void main(String[] args) {
 
+        Animal a;
+        
+        a = new Dog();
+        a.whoAmI();
+
         Dog d = new Dog();
         d.whoAmI();
 
