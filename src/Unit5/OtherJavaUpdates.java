69acf47463378c20f8987f94a5b472425cb383f3
brian chiem <brianfamily5@gmail.com>
2021-01-02 19:02:03 -0700

    Learn new updates for jdk 10

---
+++ src/Unit5/OtherJavaUpdates.java
@@ -0,0 +1,16 @@
+package JavaSampleProject1.src.Unit5;
+
+public class OtherJavaUpdates {
+
+    public static void main(String[] args) {
+
+        /*
+         * 1. Java Object Heap allocation possivle on alternative memory devices
+         *
+         * 2. Announces 6 months release model
+         * 
+         * 3. Provides default set of root Certification Authorities
+         * 
+         */ 
+    }    
+}
