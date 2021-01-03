435ee280b719ac7b0a671f6a217ab3c0710ef2e0
brian chiem <brianfamily5@gmail.com>
2021-01-02 19:23:08 -0700

    Learn method overriding

---
+++ src/Unit6/AnimalMain.java
@@ -0,0 +1,14 @@
+package JavaSampleProject1.src.Unit6;
+
+public class AnimalMain {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.whoAmI();
+
+        Cat c = new Cat();
+        c.whoAmI();
+    }
+    
+}
