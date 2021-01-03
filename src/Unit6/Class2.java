e141c98c7f3fe01aca96b3148ac0c108503c6770
brian chiem <brianfamily5@gmail.com>
2021-01-02 19:07:25 -0700

    Create class 2 for inheritance

---
+++ src/Unit6/Class2.java
@@ -0,0 +1,11 @@
+package JavaSampleProject1.src.Unit6;
+
+public class Class2 extends Class1 {
+
+
+    public void doSubtraction() {
+        System.out.println("Subtraction is " + (num1 - num2));
+
+    }
+    
+}
