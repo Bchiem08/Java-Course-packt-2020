ce442917e034677c10385490712c9c0b7bf0ed26
brian chiem <brianfamily5@gmail.com>
2021-01-03 19:41:59 -0700

    Learn final class

---
+++ src/Unit6/FinalClass2.java
@@ -0,0 +1,5 @@
+package JavaSampleProject1.src.Unit6;
+
+public class FinalClass2 extends FinalKeyword{
+    
+}
---
+++ src/Unit6/FinalKeyword.java
@@ -0,0 +1,16 @@
+package JavaSampleProject1.src.Unit6;
+
+public class FinalKeyword {
+        /**
+         * Final Field is used to create a Constant
+         * Final Method is used to avoid Method Overriding
+         * Final Class is used to prevent Inheritance
+         */
+
+    final int A = 100;
+
+    final void abc() {
+        System.out.println("Final method abc");
+    }
+
+}
