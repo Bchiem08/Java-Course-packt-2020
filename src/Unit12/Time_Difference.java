c58c90e80050820ee93b6471a26f3b1f837978f4
brian chiem <brianfamily5@gmail.com>
2021-01-07 20:08:42 -0700

    Learn time differnce

---
+++ src/Unit12/Time_Difference.java
@@ -0,0 +1,21 @@
+package JavaSampleProject1.src.Unit12;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class Time_Difference {
+
+    public static void main(String[] args) {
+
+        LocalTime t1 = LocalTime.of(19, 15);
+
+        Duration d = Duration.between(t1, LocalTime.now());
+
+        System.out.println(d);
+
+        System.out.println(d.toHours());
+        System.out.println(d.toMinutes());
+
+    }
+    
+}
