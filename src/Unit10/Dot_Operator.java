f7e021ca3a0c83ed04aa27425b10127567593600
brian chiem <brianfamily5@gmail.com>
2021-01-07 17:56:48 -0700

    Learn dot operator

---
+++ src/Unit10/Dot_Operator.java
@@ -0,0 +1,23 @@
+package JavaSampleProject1.src.Unit10;
+
+public class Dot_Operator {
+
+    public static void main(String[] args) {
+		
+		/*
+		 * (DOT) '.' -> Matches Any Single Character
+        */
+
+        String patt = "ABC.";
+
+        String str1 = "ABC9";
+
+        String patt1 = "ABC.*";
+
+        String str2 = "ABC^&*IJHG987";
+
+        System.out.println(str2.matches(patt1));
+
+    }
+    
+}
