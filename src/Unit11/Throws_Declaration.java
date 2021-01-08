d24b7d5d327b69c8af77ea54d6af183c06800bd9
brian chiem <brianfamily5@gmail.com>
2021-01-07 19:44:19 -0700

    Learn Throw Declaration

---
+++ src/Unit11/Throws_Declaration.java
@@ -0,0 +1,18 @@
+package JavaSampleProject1.src.Unit11;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Throws_Declaration {
+
+    public static void main(String[] args) throws IOException {
+		
+		abc();
+	}
+	
+	public static void abc() throws IOException {
+		FileInputStream x = new FileInputStream("Users⁩/md/⁨Downloads⁩/abc.txt");
+	}
+    
+}
