a7f368ef75d14f9fccfcb60ededc2836846087d9
brian chiem <brianfamily5@gmail.com>
2021-01-07 20:05:16 -0700

    Leanr Time add and subtract

---
+++ src/Unit12/Time_Add_Subtract.java
@@ -0,0 +1,21 @@
+package JavaSampleProject1.src.Unit12;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+public class Time_Add_Subtract {
+
+    public static void main(String[] args) {
+		
+		/*
+		 * Plus / Minus
+		 * ChronoUnit
+		 */
+
+        LocalTime t1 = LocalTime.now().plus(5, ChronoUnit.MINUTES);
+
+        System.out.println(t1);
+
+    }
+    
+}
